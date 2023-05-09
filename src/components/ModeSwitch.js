b04206af9e56b1426c660d28cbdbbdd8d56f0f38
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-05-10 01:59:31 +0300

    responsive

--- src/components/ModeSwitch.js
+++ src/components/ModeSwitch.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const ModeSwitch = ({ darkMode, handleToggle }) => {
+const ModeSwitch = ({ handleToggle }) => {
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+    handleToggle();
+  };
+
   return (
     <div className="modeSwitch">
       <label className="toggle-switch">
         <input
           type="checkbox"
           className="toggle-switch-checkbox"
-          onChange={handleToggle}
-          checked={darkMode}
+          onChange={toggleDarkMode}
+          checked={!darkMode}
         />
         <span className="slider round"></span>
       </label>
-      <div className="modeText">{darkMode ? "LIGHT MODE" : "DARK MODE"}</div>
+      <div className="modeText">{darkMode ? "DARK MODE" : "LIGHT MODE"}</div>
       <div className="modeText">|</div>
       <div className="modeText">
         <span id="lang">TÜRKÇE</span>'YE GEÇ
